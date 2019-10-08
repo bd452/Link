9e9f5c6f4d9e6b99488af8498feb242b1adeaa9c
Bryce Dougherty <bryce.dougherty@gmail.com>
2019-10-08 19:51:42 -0400

    Initial Commit

---
+++ Propel/Propel.h
@@ -0,0 +1,19 @@
+//
+//  Propel.h
+//  Propel
+//
+//  Created by Bryce Dougherty on 10/8/19.
+//  Copyright © 2019 The Farm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Propel.
+FOUNDATION_EXPORT double PropelVersionNumber;
+
+//! Project version string for Propel.
+FOUNDATION_EXPORT const unsigned char PropelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Propel/PublicHeader.h>
+
+
