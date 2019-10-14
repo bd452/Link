b8dbc56e12a1a9ca60294071ff855e4709c55271
Bryce Dougherty <bryce.dougherty@retailbusinessservices.com>
2019-10-14 16:46:37 -0400

    Add Action listeners Add attributes to button Add Attributed String

--- Propel/UI/Button.swift
+++ Propel/UI/Button.swift
@@ -11,12 +11,12 @@ import UIKit
 public typealias Button = UIButton
 
 public extension Button {
-    func title(_ title: String, for state: UIControl.State = .normal) -> Self {
+    func title(_ title: String, forState state: UIControl.State = .normal) -> Self {
         self.setTitle(title, for: state)
         return self
     }
-    func color(_ color: Color) -> Self {
-        self.titleLabel?.textColor = color
+    func color(_ color: Color, forState state: UIControl.State = .normal) -> Self {
+        self.setTitleColor(color, for: state)
         return self
     }
     func withTitleLabel(_ label: (Label?)->Void) -> Self {
@@ -26,5 +26,26 @@ public extension Button {
     func font(_ font: Font) -> Self {
         self.titleLabel?.font = font
         return self
+    }
+    func image(_ image: UIImage, forState state:UIControl.State = .normal) -> Self {
+        self.setImage(image, for: state)
+        return self
+    }
+    func backgroundImage(_ image: UIImage?, forState state: UIControl.State = .normal) -> Self {
+        self.setBackgroundImage(image, for: state)
+        return self
+    }
+    func target(_ target: Any, action: Selector, forControlEvents events: UIControl.Event) -> Self {
+        self.addTarget(target, action: action, for: events)
+        return self
+    }
+    func action(_ action: @escaping (UIView?) -> Void, forControlEvents events: UIControl.Event) -> Self {
+        let sel = ActionSelector(action)
+        self.addTarget(sel, action: #selector(ActionSelector.action(sender:)), for: events)
+        return self
+    }
+    func addAction(_ action: @escaping (UIView?) -> Void, forControlEvents events: UIControl.Event) {
+        let sel = ActionSelector(action)
+        self.addTarget(sel, action: #selector(ActionSelector.action(sender:)), for: events)
     }
 }
---
+++ libPropel/libPropel.swift
@@ -0,0 +1,11 @@
+//
+//  libPropel.swift
+//  libPropel
+//
+//  Created by BRYCE P DOUGHERTY on 10/14/19.
+//  Copyright © 2019 The Farm. All rights reserved.
+//
+
+class libPropel {
+
+}
